ffc8b8bd3a751ed755b23b6eedcf39205835d7e4
Ezequiel Araújo Dos Santos <85441647+ryze07@users.noreply.github.com>
2024-04-08 18:27:56 -0300

    Delete exerci.py

--- exerci.py
+++
@@ -1,8 +0,0 @@
-primeiro_valor = int(input('Digite um valor '))
-segundo_valor = int(input('Digite outro valor '))
-
-if primeiro_valor > segundo_valor:
-    print(f'O valor {primeiro_valor} é maior que o {segundo_valor}')
-
-else:
-    print(f'O valor {segundo_valor} é maior que o {primeiro_valor} ')
